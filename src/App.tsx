98ecf694feb26eabc3bb325cb1c4c54a0afd7d74
Ma-Der <eco_derma@poczta.onet.pl>
2023-10-01 12:37:00 +0200

    chore: init

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import React, { Fragment } from 'react';
+import './App.scss';
+import { Routes, Route } from 'react-router-dom';
+
+function App() {
+  return (
+    <Fragment>
+
+
+<div >
+
+</div>
+    </Fragment>
+    
+  );
+}
+
+export default App;
---
+++ src/components/Navigation/index.tsx
@@ -0,0 +1,5 @@
+import styles from './rwd.module.scss';
+
+export const Navigation = () => {
+  return <div></div>;
+};
