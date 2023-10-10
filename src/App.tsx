bbb696e4bc61fb0305181da20cf2e1a19fa5c668
Ma-Der <eco_derma@poczta.onet.pl>
2023-10-10 19:49:18 +0200

    chore: content styling

--- src/App.tsx
+++ src/App.tsx
@@ -14,18 +14,30 @@ function App() {
   const [activeLink, setActiveLink] = useState("Home");
   const ref = useRef<HTMLDivElement | null>(null);
   const { height } = useFooterDimensions(ref);
-  
+
   return (
     <Fragment>
-      <Header activeLink={activeLink} setActiveLink={setActiveLink}/>
+      <Header activeLink={activeLink} setActiveLink={setActiveLink} />
       <MobileHeader activeLink={activeLink} setActiveLink={setActiveLink} />
-      <Routes>
-        <Route path={"/"} element={<Homepage footerHeight={height}/>} />
-        <Route path={"/contact"} element={<Contact footerHeight={height}/>} />
-        <Route path={"/hobby"} element={<Hobby footerHeight={height}/>} />
-        <Route path={"/projects"} element={<Projects footerHeight={height}/>} />
-      </Routes>
-      <Footer activeLink={activeLink} setActiveLink={setActiveLink} footerRef={ref}/>
+      <div className="wrapper">
+        <Routes>
+          <Route path={"/"} element={<Homepage footerHeight={height} />} />
+          <Route
+            path={"/contact"}
+            element={<Contact footerHeight={height} />}
+          />
+          <Route path={"/hobby"} element={<Hobby footerHeight={height} />} />
+          <Route
+            path={"/projects"}
+            element={<Projects footerHeight={height} />}
+          />
+        </Routes>
+      </div>
+      <Footer
+        activeLink={activeLink}
+        setActiveLink={setActiveLink}
+        footerRef={ref}
+      />
     </Fragment>
   );
 }
