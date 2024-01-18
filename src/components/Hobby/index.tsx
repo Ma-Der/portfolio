b958c4c0e1e666963059a829f63a3c7516d9cb7e
Ma-Der <eco_derma@poczta.onet.pl>
2024-01-18 14:34:22 +0100

    add images to hobby, fix fonts

--- src/components/Hobby/index.tsx
+++ src/components/Hobby/index.tsx
@@ -1,10 +1,15 @@
 import { HobbyProps } from "../../contracts/components/Hobby";
+import styles from './base.module.scss';
 
-export const Hobby = ({ title, description }: HobbyProps) => {
+const { wrapper, wrapperImage } = styles;
+
+export const Hobby = ({ title, description, images }: HobbyProps) => {
   return (
-    <div>
+    <div className={wrapper}>
       <h3>{title}</h3>
       <p>{description}</p>
+      <img className={wrapperImage} src={images[0]} alt="Me&Guitar" />
+      <img className={wrapperImage} src={images[1]} alt="Stage" />
     </div>
   );
 };
--- src/contracts/components/Hobby/index.ts
+++ src/contracts/components/Hobby/index.ts
@@ -1,4 +1,5 @@
 export type HobbyProps = {
   title: string;
   description: string;
+  images: string[];
 };
